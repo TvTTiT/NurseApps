7aab8648aa5c8b1a8927bdd9c7554ac4793307b1
TvTTiT <thanhtvfs0830@gmail.com>
2023-05-28 04:54:53 +0200

    update and adding new styles

--- screens/homeScreens/ChangeEmailScreen.js
+++ screens/homeScreens/ChangeEmailScreen.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from '../../styles/homeStyles/ChangeNameStyles'; 
-
+import { Ionicons } from '@expo/vector-icons';
 const ChangeEmailScreen = ({navigation }) => {
   const [email, setEmail] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -16,6 +16,11 @@ const ChangeEmailScreen = ({navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => changeMind()}>
+          <Ionicons name="arrow-back" size={24} color="#000" style={styles.goBackIcon} />
+        </TouchableOpacity>
+      </View>
       <Text style={styles.title}>Changing Email</Text>
       <View style={styles.inputView}>
         <TextInput
