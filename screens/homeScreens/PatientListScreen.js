3b61fe5c8fd57a3dc3a3584924b4946f7eef2fdb
TvTTiT <thanhtvfs0830@gmail.com>
2023-06-07 05:49:41 +0200

    update Patients with api

--- screens/homeScreens/PatientListScreen.js
+++ screens/homeScreens/PatientListScreen.js
@@ -1,47 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { styles } from '../../styles/homeStyles/PatientListStyles';
-
-const mockPatients = [
-  { id: 1, name: 'John Smith'},
-  { id: 2, name: 'Jane Doe'},
-  { id: 3, name: 'Bob Johnson'},
-  { id: 4, name: 'Samantha Lee'},
-  { id: 5, name: 'Michael Brown'},
-  { id: 6, name: 'John Smith'},
-  { id: 7, name: 'Jane Doe'},
-  { id: 8, name: 'Bob Johnson'},
-  { id: 9, name: 'Samantha Lee'},
-  { id: 10, name: 'Michael Brown'},
-];
+import { supabase } from '../../lib/supabaseConfig';
 
 const PatientListScreen = ({ navigation }) => {
   const [searchText, setSearchText] = useState('');
-  const [filteredPatients, setFilteredPatients] = useState(mockPatients);
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  const fetchPatients = async () => {
+    try {
+      let { data: patients, error } = await supabase
+        .from('patients')
+        .select('patient_id,first_name,last_name');
+      
+      if (error) {
+        console.error('Error fetching patients:', error);
+        return;
+      }
+  
+      setPatients(patients);
+    } catch (error) {
+      console.error('Error fetching patients:', error);
+    }
+  };
 
   const handleSearch = (text) => {
-    const filtered = mockPatients.filter(
+    const filtered = patients.filter(
       (patient) =>
-        patient.name.toLowerCase().indexOf(text.toLowerCase()) !== -1
+        patient.first_name.toLowerCase().indexOf(text.toLowerCase()) !== -1 ||
+        patient.last_name.toLowerCase().indexOf(text.toLowerCase()) !== -1
     );
-    setFilteredPatients(filtered);
     setSearchText(text);
+    setFilteredPatients(filtered);
   };
 
   const navigateToPatientDetail = (item) => {
-    //navigation.navigate('Patient Detail', { patient: item });
     navigation.navigate('PatientData', { patient: item });
   };
 
   const renderPatient = ({ item }) => {
+    const fullName = `${item.first_name} ${item.last_name}`;
     return (
       <TouchableOpacity
         style={styles.patientItem}
         onPress={() => navigateToPatientDetail(item)}
       >
         <View style={styles.patientShape}>
-          <Text style={styles.patientName}>{item.name}</Text>
+          <Text style={styles.patientName}>{fullName}</Text>
           <MaterialCommunityIcons name="chevron-right" size={24} color="#fff" />
         </View>
       </TouchableOpacity>
@@ -60,9 +70,9 @@ const PatientListScreen = ({ navigation }) => {
         />
       </View>
       <FlatList
-        data={filteredPatients}
+        data={patients}
         renderItem={renderPatient}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item) => item.patient_id.toString()}
         style={styles.list}
       />
     </View>
