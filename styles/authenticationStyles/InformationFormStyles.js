1119c2c7ebf91cfeaf2a8335f9b26bcc8327c843
TvTTiT <thanhtvfs0830@gmail.com>
2023-06-09 23:55:32 +0200

    new form

---
+++ styles/authenticationStyles/InformationFormStyles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  form: {
+    width: '80%',
+  },
+  label: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 8,
+    marginBottom: 12,
+    fontSize: 14,
+  },
+  saveButton: {
+    backgroundColor: '#fb5b5a',
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  saveButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  cancelButton: {
+    backgroundColor: '#ccc',
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 10,
+  },
+  cancelButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+});
