22af72e8959a918d7854223d00c84324be1fa373
TvTTiT <thanhtvfs0830@gmail.com>
2023-06-09 19:10:29 +0200

    api with name

--- screens/homeScreens/ChangeEmailScreen.js
+++ screens/homeScreens/ChangeEmailScreen.js
@@ -1,18 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { styles } from '../../styles/homeStyles/ChangeNameStyles'; 
+import { styles } from '../../styles/homeStyles/ChangeNameStyles';
 import { Ionicons } from '@expo/vector-icons';
-const ChangeEmailScreen = ({navigation }) => {
+import { supabase } from '../../lib/supabaseConfig';
+import { UserContext } from '../../App';
+
+const ChangeEmailScreen = ({ navigation, route }) => {
+  const Email = route.params?.email;
+  const { medicalProfessionalId, userID } = useContext(UserContext);
   const [email, setEmail] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const confirm = () => {
-    // Perform sign-up logic here
-  }
+  const updateMedicalEmail = async () => {
+    try {
+      const { error } = await supabase
+        .from('medicalprofessionals')
+        .update({ email })
+        .eq('medical_professional_id', medicalProfessionalId[0].medical_professional_id);
 
-  const goBack = () => {
-    navigation.navigate('PersonalDetails');
+      if (error) {
+        console.error('Error updating medical email:', error);
+        return;
+      }
+      console.log('Medical Email updated successfully');
+      updateUserEmail();
+    } catch (error) {
+      console.error('Error updating email:', error);
+    }
+  };
+  const updateUserEmail = async () => {
+    try {
+      const { error } = await supabase
+        .from('users')
+        .update({ email })
+        .eq('user_id', userID);
+
+      if (error) {
+        console.error('Error user updating email:', error);
+        return;
+      }
+      alert('User Email updated successfully');
+      setConfirmPassword('');
+      setEmail('');
+      navigation.navigate('PersonalDetails');
+    } catch (error) {
+      console.error('Error updating user email:', error);
+    }
+  };
+  const handleUpdateEmail = async () => {
+    try {
+      let { data: user_password, error } = await supabase
+        .from('users')
+        .select('password')
+        .eq('user_id', userID);
+
+      if (error) {
+        console.error('Error selecting password:', error);
+        alert('Error selecting password');
+        return;
+      }
+      const userPassword = user_password[0]?.password;
+      if(confirmPassword == userPassword) {
+        updateMedicalEmail();
+      }else{
+        alert('Incorrect password!!!');
+      }
+    } catch (error) {
+      console.error('Error selecting email:', error);
+    }
   }
+  const goBack = () => {
+    setConfirmPassword('');
+    setEmail('');
+    navigation.navigate('PersonalDetails');
+  };
 
   return (
     <View style={styles.container}>
@@ -25,7 +86,7 @@ const ChangeEmailScreen = ({navigation }) => {
       <View style={styles.inputView}>
         <TextInput
           style={styles.inputText}
-          placeholder="Email..."
+          placeholder={Email}
           placeholderTextColor="#003f5c"
           value={email}
           onChangeText={(text) => setEmail(text)}
@@ -41,16 +102,14 @@ const ChangeEmailScreen = ({navigation }) => {
           onChangeText={(text) => setConfirmPassword(text)}
         />
       </View>
-      <TouchableOpacity style={styles.signupBtn} onPress={confirm}>
+      <TouchableOpacity style={styles.signupBtn} onPress={handleUpdateEmail}>
         <Text style={styles.signupText}>Confirm</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={changeMind}>
+      <TouchableOpacity onPress={goBack}>
         <Text style={styles.back}>Changing your mind? click here</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-
 
 export default ChangeEmailScreen;
